2b8c93b1a17a451831320b85dc698d940421835f
sh13ldxx <tu7eay65k@code.edu.az>
2022-05-19 07:09:12 -0400

    tst

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,26 +4,39 @@ jQuery(document).ready(function($) {
             
             return RandomNumber = Math.floor((Math.random() * 250) + 1);
         }
+
         // console.log(data);   
         GetRandomNumber()
         var RandomJson = data[RandomNumber]
         console.log(RandomJson);
         var TruAnswer = RandomJson.name.common
         GetRandomNumber()
-        var RandomJson2 = data[RandomNumber]
-        var RandomJson3 = data[RandomNumber]
-        var RandomJson4 = data[RandomNumber]
+        var RandomJson2 = data[RandomNumber].name.common
+        var RandomJson3 = data[RandomNumber].name.common
+        var RandomJson4 = data[RandomNumber].name.common
                 $(".test").html(`
                 <img src="${RandomJson.flags.png}" alt="">
-                <form id="form"; style= "text-align:center">
-                <input type="radio" name="scores" id="" value="1"> ${RandomJson2.name.common}
-                <input type="radio" name="scores" id="4" value="4"> ${TruAnswer}
-                <input type="radio" name="scores" id="2" value="2">${RandomJson3.name.common}
-                <input type="radio" name="scores" id="3" value="3"> ${RandomJson4.name.common}
+                <form id="FormSend" style= "text-align:center"'>
+                <input type="radio" name="scores" checked id="1" value="1"> ${TruAnswer}
+                <input type="radio" name="scores" id="2" value="2"> ${RandomJson2}
+                <input type="radio" name="scores" id="3" value="3"> ${RandomJson3}
+                <input type="radio" name="scores" id="4" value="4"> ${RandomJson4}
                 <input type="submit" name="mysubmit" value="Submit"/>
             </form>
                 `);
-                var SeletcedAnswer = document.querySelector('input[name="scores"]:checked').value;
-                console.log(SeletcedAnswer);
-        });
+                $("#FormSend").submit(function( event ) {
+                        var score = document.querySelector('input[name="scores"]:checked').value;
+                        if(!score){
+                          alert('No score was selected. Try again.');
+                          return false;
+                        }
+                        else{                            
+                          if (score==1) {
+                              alert("True Answer")
+                          }else{
+                              alert("Wrong answer")
+                          }
+                        }
+                  });
+     } );
     });
